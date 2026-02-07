e89a47044711867a0846611fe0da1f5a22b6b9b8
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2026-02-07 20:19:57 +0100

    Feat: add the attribute section of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 // import { useState } from "react";
+import Attribute from "./components/Attribute.jsx";
+// import data from "./json/data.json";
 // import iconMemory from "/images/icon-memory.svg";
 // import icoRreaction from "/images/icon-reaction.svg";
 // import iconVerbal from "/images/icon-verbal.svg";
@@ -8,7 +10,19 @@ import "./App.css";
 function App() {
   // const [count, setCount] = useState(0);
 
-  return <></>;
+  return <>
+  <div className="container">
+    <div className="score-container">
+      <p></p>
+      <div className="circle">
+        <h1 className="score">76</h1>
+        <p className="total">of 100</p>
+      </div>
+    </div>
+  </div>
+  <Attribute/>
+  </>;
+
 }
 
 export default App;
---
+++ src/components/Attribute.jsx
@@ -0,0 +1,17 @@
+function Attribute() {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noopener noreferrer">
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/SamIfesi" target="_blank" rel="noopener noreferrer">
+        Eke, ifesinachi Samrose
+      </a>
+      .
+    </div>
+  );
+}
+
+export default Attribute;
