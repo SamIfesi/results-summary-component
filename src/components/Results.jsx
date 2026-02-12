7ac83795b3af02d3e9ec1a4c349de866d5179b35
SamIfesi <Ekesamrose@gmail.com>
2026-02-12 10:49:16 +0100

    style: enhance test type styling with distinct background colors and text colors

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,7 +4,7 @@ export default function Results() {
   return (
     <>
       {data.map((item) => (
-        <div className="test-type" key={item.category}>
+        <div className={`test-type ${item.category.toLowerCase()}`} key={item.category}>
           <div className="test">
             <img src={item.icon} alt={item.category} />
             <p>{item.category}</p>
